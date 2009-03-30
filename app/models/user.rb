10ba5495a1ad398714b1270c3907c6b4d15a3579
Christian Amor Kvalheim <christkv@gmail.com>
2009-03-30 14:19:45 +0200

    Added reset password function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   #  c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
   #end # block optional  
   acts_as_authentic# :crypto_provider => Authlogic::CryptoProviders::BCrypt
+  
+  def deliver_password_reset_instructions!
+    reset_perishable_token!
+    Notifier.deliver_password_reset_instructions(self)
+  end  
 end
